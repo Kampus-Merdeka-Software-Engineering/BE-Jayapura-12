ad9f663c97752eb3447c66020347def66c819f30
Teguh Karya Rizki <134355459+Teguhkr@users.noreply.github.com>
2023-10-09 08:37:03 +0700

    feat(deploy): add database and backend deployment scripts

--- app/model/index.js
+++ app/model/index.js
@@ -1,8 +1,10 @@
 const sequelize = require('sequelize')
 const mysql = require('mysql2')
+const fs = require('fs')
+const path = require('path')
 
-const connection = new sequelize.Sequelize('revou-jayapura', 'root', 'Teguh0822', {
-    host: 'localhost',
+const connection = new sequelize.Sequelize("mysql://avnadmin:AVNS_JdQ1fcfZHRMiJSsc6UC@mysql-group12-group12.aivencloud.com:20580/defaultdb?ssl-mode=REQUIRED", {
+    ssl: fs.readFileSync(path.join(__dirname,'../../mysql-group12 ca.pem')),
     dialect: 'mysql',
     logging: false,
   })
